70aa0b36501648579dcee1c60dccc57a4d29c270
ambition <nibainle@gamil.com>
2020-11-09 15:52:30 +0800

    hello

---
+++ json/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+
+	"fmt"
+)
+
+func main() {
+	testStruct()
+	testMap()
+}
+
+type Monster struct {
+	Name     string
+	Age      int
+	Brithday string
+	Sal      float64
+	Skill    string
+}
+
+func testStruct() {
+	monster := Monster{
+		"haha",
+		599,
+		"1999-11-11",
+		11.23,
+		"hah",
+	}
+	data, err := json.Marshal(monster)
+	if err != nil {
+		fmt.Println("wrong")
+	}
+	fmt.Println(string(data))
+}
+func testMap() {
+	m := make(map[string]int)
+	m["hello"] = 1
+	m["nihao"] = 2
+	data, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("err")
+	}
+	fmt.Println(string(data))
+}
